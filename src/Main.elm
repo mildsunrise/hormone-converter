ad09e08709afd24c51e9504c74e9617a23f29e13
Alba Mendez <me@jmendeth.com>
2018-06-04 01:40:38 +0200

    (2/2) Pass widths from JS side

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,34 @@
-import Html exposing (beginnerProgram)
-import Converter exposing (model, view, update)
+port module Main exposing (..)
 
-main = Html.beginnerProgram { model = model, view = view, update = update }
+import Html exposing (beginnerProgram)
+import Converter
+
+port viewWidths : (Float -> msg) -> Sub msg
+
+main = Html.program
+  { init = init
+  , view = view
+  , update = update
+  , subscriptions = subscriptions
+  }
+
+-- MODEL
+
+type alias Model = Converter.Model
+
+init : (Model, Cmd Message)
+init = (Converter.model, Cmd.none)
+
+-- UPDATE
+
+type alias Message = Converter.Message
+
+update msg model = (Converter.update msg model, Cmd.none)
+
+-- SUBSCRIPTIONS
+
+subscriptions model = viewWidths Converter.SetWidth
+
+-- VIEW
+
+view = Converter.view
